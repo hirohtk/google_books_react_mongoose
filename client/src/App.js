c19a2a18c0a9bb0e83e85ebeb5c93c531b6c3f17
Kensen <hirohtk@gmail.com>
2020-01-23 20:53:14 -0800

    refactoring to put stateful component the app instead of the search.js file seems to be successful.  just changed the search.js to accept props of the same name, now at app level

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,26 @@ class App extends React.Component {
 
   }
 
+  search = (query) => {
+    console.log(`Searching for ${query}...`)
+  }
+
+  // the onChange JS handler that's in the form triggers this method below, which changes state every time a key is pressed
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(`name is ${name}`)
+    console.log(`value is ${value}`)
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.search(this.state.searchTerm);
+  };
+
   render() {
     return (
       <div>
@@ -22,7 +42,12 @@ class App extends React.Component {
               <h3>(React) Google Books Search</h3>
               <h4>Search for and Save Books of interest!</h4>
             </div>
-            <Search></Search>
+            <Search
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+            value={this.searchTerm}
+            >
+            </Search>
           </div>
           <div className="row">
             <div className="results">
